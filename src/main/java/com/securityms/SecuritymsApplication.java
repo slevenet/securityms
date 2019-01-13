638ded6fb6b2a790ce585819249ef39298c6a75f
slevenet <ser.levenetc@gmail.com>
2019-01-13 13:36:28 +0300

    initial

---
+++ src/main/java/com/securityms/SecuritymsApplication.java
@@ -0,0 +1,14 @@
+package com.securityms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritymsApplication.class, args);
+    }
+
+}
+
